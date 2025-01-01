f065301c7cc3a8e73ee311be74f2b446f1952f5a
bdeshev <grievous@abv.bg>
2025-01-01 21:05:11 +0200

    Fix idle animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ enum MoveDirection {NONE, UP, DOWN, LEFT, RIGHT}
 enum AnimationType {WALK, IDLE}
 
 const speed = 100
+var direction: MoveDirection = MoveDirection.NONE
 
 func _physics_process(delta: float):
 	player_movement(delta )
@@ -11,29 +12,33 @@ func _physics_process(delta: float):
 
 func player_movement(_delta):
 	if Input.is_action_pressed("ui_right"):
-		play_anim(MoveDirection.RIGHT, AnimationType.WALK)
+		direction = MoveDirection.RIGHT
+		play_anim(AnimationType.WALK)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
-		play_anim(MoveDirection.LEFT, AnimationType.WALK)
+		direction = MoveDirection.LEFT
+		play_anim(AnimationType.WALK)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
-		play_anim(MoveDirection.DOWN, AnimationType.WALK)
+		direction = MoveDirection.DOWN
+		play_anim(AnimationType.WALK)
 		velocity.x = 0
 		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
-		play_anim(MoveDirection.UP, AnimationType.WALK)
+		direction = MoveDirection.UP
+		play_anim(AnimationType.WALK)
 		velocity.x = 0
 		velocity.y = -speed
 	else:
-		play_anim(MoveDirection.NONE, AnimationType.IDLE)
+		play_anim(AnimationType.IDLE)
 		velocity.x = 0
 		velocity.y = 0
 
 	move_and_slide()
 
-func play_anim(direction: MoveDirection, type: AnimationType):
+func play_anim(type: AnimationType):
 	print("direction: %s, type: %s" % [str(direction), str(type)])
 	var anim = $AnimatedSprite2D
 
@@ -65,4 +70,4 @@ func play_anim(direction: MoveDirection, type: AnimationType):
 		if type == AnimationType.WALK:
 			anim.play("back_walk")
 		elif type == AnimationType.IDLE:
-			anim.play("back_idle")
+			anim.play("front_idle")
